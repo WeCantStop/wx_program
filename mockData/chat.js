27c8a638d9645387667ad1858965176aabe2880a
Minmin Tang <minmin.tang@careerbuilder.com>
2018-03-13 14:08:23 +0800

    add map and scroll-y to chat tab

--- mockData/chat.js
+++ mockData/chat.js
@@ -1,8 +1,7 @@
 var chatPlace = [
-  { img: 'http://m.qpic.cn/psb?/V13sys0K3RoUvw/9lEZrHHL1b3WHyL.dQYlEa1tgd5VWmDkx4vSNKVGynA!/b/dPQAAAAAAAAA&bo=gAJXAwAAAAARB.Y!&rf=viewer_4'},
-  { img: 'http://m.qpic.cn/psb?/V13sys0K3RoUvw/MyxiG.hQMaGCkdI5M*zwP2g41ZpR3ZNkGIoTrk9KSrA!/b/dIUBAAAAAAAA&bo=gAJXAwAAAAARB.Y!&rf=viewer_4'},
-  { img: 'http://m.qpic.cn/psb?/V13sys0K3RoUvw/98v.99qD0GVOq.*.CMVUYKDg189avAL9*DPJn8u8TiY!/b/dIUBAAAAAAAA&bo=gAJvBAAAAAARB9k!&rf=viewer_4'},
-  { img: 'http://m.qpic.cn/psb?/V13sys0K3RoUvw/OgVIkZCgHWXQj8KepjH6Xv71EO4VGceN7QSQ9qkMSio!/b/dF4BAAAAAAAA&bo=gAJVAwAAAAARB.Q!&rf=viewer_4'}
+  { img: '../../../assets/imgs/common/featureDog.jpg'},
+  { img: '../../../assets/imgs/common/pinkDog.jpeg'},
+  { img: '../../../assets/imgs/common/whiteDog.jpg'}
 ]
 
 module.exports = {chatPlace}
